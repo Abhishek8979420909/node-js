0af50f2f96fa441a5b75e0c709a1a1501ecd6f2f
Abhisehk Rawat <162818246+Abhishek8979420909@users.noreply.github.com>
2024-09-03 02:00:18 +0000

    Add two number with node js

---
+++ index.js
@@ -0,0 +1,8 @@
+// let a = 10;
+// let b = 11;
+// let result = a + b;
+// console.log(`The Sum of ${a} and ${b} is ${result}`);
+let a = parseInt(process.argv[2]);
+let b = parseInt(process.argv[2+1]);
+let result = a + b;
+console.log(`The Sum of ${a} and ${b} is ${result}`);
